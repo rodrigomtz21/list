13b03270ee71568f0bc91a4cf512458b2c6a77f0
recrearodrigoh <rodrigo.mtz.hrz@gmail.com>
2020-02-16 19:46:47 -0600

    first commit

---
+++ precache-manifest.76079b2699a359d5ccdada30d71c57ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e80f1927dae8a637b9bdf9757c728447",
+    "url": "./index.html"
+  },
+  {
+    "revision": "860cc197579641d38244",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7c416e669c6c88435212",
+    "url": "./static/css/main.bf72d5db.chunk.css"
+  },
+  {
+    "revision": "860cc197579641d38244",
+    "url": "./static/js/2.6d5e194b.chunk.js"
+  },
+  {
+    "revision": "9f2c89b6b3d1c28fc84568b52ca3493b",
+    "url": "./static/js/2.6d5e194b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c416e669c6c88435212",
+    "url": "./static/js/main.615119b4.chunk.js"
+  },
+  {
+    "revision": "3a770439a0a8a3427cb2",
+    "url": "./static/js/runtime-main.04b1b2d8.js"
+  }
+]);
